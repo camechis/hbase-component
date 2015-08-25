5e5917171067fb2f59eb4a216768579f1a72c27f
Travis Camechis <camechis@gmail.com>
2015-08-24 22:17:14 -0400

    added multimethod to convert from type back into data type

--- src/camechis/component/hbase/byte_utils.clj
+++ src/camechis/component/hbase/byte_utils.clj
@@ -4,3 +4,26 @@
 
 (defn to-bytes [x]
   (Bytes/toBytes x))
+
+(defmulti from-bytes :type )
+
+(defmethod from-bytes :string
+  [x]
+  (Bytes/toString (:bytes x)))
+
+(defmethod from-bytes :int
+  [x]
+  (Bytes/toInt (:bytes x)))
+
+(defmethod from-bytes :float
+  [x]
+  (Bytes/toFloat (:bytes x)))
+
+(defmethod from-bytes :double
+  [x]
+  (Bytes/toDouble (:bytes x)))
+
+
+(defmethod from-bytes :long
+  [x]
+  (Bytes/toLong (:bytes x)))
